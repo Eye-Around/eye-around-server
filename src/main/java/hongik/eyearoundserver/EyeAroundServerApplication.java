bad368979222ee53fd893d5d38baec3172257122
yesjjin99 <yesjjin99@naver.com>
2023-05-11 16:36:24 +0900

    Chore: Project Init

---
+++ src/main/java/hongik/eyearoundserver/EyeAroundServerApplication.java
@@ -0,0 +1,13 @@
+package hongik.eyearoundserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyeAroundServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyeAroundServerApplication.class, args);
+	}
+
+}
