b4983dbe08acaa6ab2bb2beab395ab7e6b229fd5
yesjjin99 <yesjjin99@naver.com>
2023-05-13 14:04:49 +0900

    Fix: Add api response error content

--- src/main/java/hongik/eyearoundserver/controller/ExerciseController.java
+++ src/main/java/hongik/eyearoundserver/controller/ExerciseController.java
@@ -3,6 +3,8 @@ package hongik.eyearoundserver.controller;
 import hongik.eyearoundserver.domain.User;
 import hongik.eyearoundserver.dto.ExerciseRequestDto;
 import hongik.eyearoundserver.dto.ExerciseResponseDTO;
+import hongik.eyearoundserver.exception.ErrorCode;
+import hongik.eyearoundserver.exception.ErrorResponse;
 import hongik.eyearoundserver.service.ExerciseService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
@@ -39,7 +41,7 @@ public class ExerciseController {
     @Operation(summary = "눈 운동 리스트 조회 API", description = "눈 운동 리스트 전체를 조회하여 반환합니다.")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(array = @ArraySchema(schema = @Schema(implementation = ExerciseResponseDTO.class)))),
-            @ApiResponse(responseCode = "401", description = "유효하지 않은 토큰입니다")
+            @ApiResponse(responseCode = "401", description = "유효하지 않은 토큰입니다", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
     @GetMapping
     public List<ExerciseResponseDTO> getExerciseList() {
@@ -50,8 +52,8 @@ public class ExerciseController {
     @Operation(summary = "눈 운동 상세 조회 API", description = "사용자가 눈 운동 리스트 중 하나를 클릭할 시, 눈 운동 정보를 반환합니다.")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(schema = @Schema(implementation = ExerciseResponseDTO.class))),
-            @ApiResponse(responseCode = "401", description = "유효하지 않은 토큰입니다"),
-            @ApiResponse(responseCode = "404", description = "요청하신 눈 운동을 찾을 수 없습니다")
+            @ApiResponse(responseCode = "401", description = "유효하지 않은 토큰입니다", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "요청하신 눈 운동을 찾을 수 없습니다", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
     @GetMapping("/{exerciseId}")
     public ResponseEntity<ExerciseResponseDTO> getExercise(@PathVariable Long exerciseId) {
@@ -61,8 +63,8 @@ public class ExerciseController {
 
     @Operation(summary = "오늘의 눈 운동 완료 API", description = "사용자가 운동 시작을 클릭할 시 요청되어야 할 API 입니다. 하루에 한 번 이상 운동을 할 경우 이번주 운동 횟수를 갱신합니다.")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "성공"),
-            @ApiResponse(responseCode = "401", description = "유효하지 않은 토큰입니다")
+            @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "401", description = "유효하지 않은 토큰입니다", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
     @PostMapping("/check")
     public ResponseEntity changeTodayState(@AuthenticationPrincipal User user) {
